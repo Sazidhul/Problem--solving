ce23000da0851606559fbb7d5e94f070e3e9a98f
Sazidhul <sazisazidhul@gmail.com>
2026-06-23 20:57:29 +0600

    Smart light pilot

---
+++ Smart Light Pilolt/Smart_light_pilot.py
@@ -0,0 +1,8 @@
+brightness, threshold = map(float, input().split())
+
+if brightness >= threshold:
+   print("ON")
+else:
+   print("OFF")
+
+#https://www.hackerrank.com/contests/ai-ml-module-4-practice-day-1/challenges/b1-smart-light-pilot-threshold/problem?isFullScreen=true
